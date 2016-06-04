235dd6fcb3699be7d5ee8629485831edba791c56
Anna Kennedy <yedennek@googlemail.com>
2016-06-04 16:00:51 +0100

    Controller tests

---
+++ spec/controllers/lights_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+describe LightsController do
+  describe "GET index" do
+    it "renders index" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it "assigns lights" do
+      get :index
+      expect(assigns(:lights)).to eq Light.all
+    end
+  end
+
+  describe "PUT update" do
+    let(:light_json) { {id: 1, name: "Office", on: true, colour: "ffffff"} }
+
+    before do
+      @light = Light.create(name: "Office", on: false, colour: "ffffff")
+    end
+
+    after do
+      @light.destroy
+    end
+
+    it "returns json of current light state" do
+      expected_json = {id: 1, name: "Office", on: true, colour: "ffffff"}.to_json
+      put :update, light_json
+      expect(response.status).to eq 200
+      expect(response.body).to eq expected_json
+    end
+  end
+end

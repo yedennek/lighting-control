53b1a3c8aa8b93569009f0e6e06864c1f08d5e84
Anna Kennedy <yedennek@googlemail.com>
2016-06-13 15:49:08 +0100

    Remove old lighting configuration on upload of new one

--- app/controllers/configuration_controller.rb
+++ app/controllers/configuration_controller.rb
@@ -13,12 +13,18 @@ class ConfigurationController < ApplicationController
   private
 
   def create_lights config
+    drop_existing_config
     controllers = config['controllers']
     controllers.each do |controller|
-      light_controller = LightGroup.create(ip_address: controller['ip'], name: controller['name'])
+      light_group = LightGroup.create(ip_address: controller['ip'], name: controller['name'])
       controller['groups'].each do |light|
-        Light.create(name: light['name'], group_number: light['number'], light_controller_id: light_controller.id)
+        Light.create(name: light['name'], group_number: light['number'], light_controller_id: light_group)
       end
     end
+  end
+
+  def drop_existing_config
+    LightGroup.destroy_all
+    Light.destroy_all
   end
 end
--- spec/controllers/configuration_controller_spec.rb
+++ spec/controllers/configuration_controller_spec.rb
@@ -29,5 +29,18 @@ describe ConfigurationController do
       expect(Light.all.size).to eq 5
       expect(LightGroup.all.size).to eq 2
     end
+
+    context "where there's existing config" do
+      before do
+        light_controller = LightGroup.create(ip_address: '0.0.0.0', name: 'house')
+        Light.create(name: 'Lounge', group_number: 3, light_controller_id: light_controller.id)
+      end
+
+      it "removes existing lights and lighting groups" do
+        post :upload, :config_file => @file
+        expect(Light.all.size).to eq 5
+        expect(LightGroup.all.size).to eq 2
+      end
+    end
   end
 end

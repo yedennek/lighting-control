d03ec5226e8e560d3775467d1dbe0d6db26097ef
Adam Whittingham <adam@devmountain.co.uk>
2016-06-18 19:19:53 +0100

    Improve CSS for low light

--- app/models/light.rb
+++ app/models/light.rb
@@ -5,10 +5,6 @@ class Light < ActiveRecord::Base
     light_group = LightGroup.find_by light_controller_id
     controller = Milight::Controller.new light_group.ip_address
 
-    if on
-      controller.group(group_number).on
-    else
-      controller.group(group_number).off
-    end
+    on ? controller.group(group_number).on : controller.group(group_number).off
   end
 end
